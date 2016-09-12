d33d485661b09c8968279aba48baa92959bcaedf
ugowe <joe@spheares.com>
2016-09-12 14:19:59 -0400

    initial commit

---
+++ CinemaClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CinemaClub
+//
+//  Created by Ugowe on 9/12/16.
+//  Copyright © 2016 Ugowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
