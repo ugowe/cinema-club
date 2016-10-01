eb9f0e4e7202e464dcc0cea8d0adf0514feb37a6
ugowe <joe@spheares.com>
2016-09-30 22:08:54 -0400

    Add Tab Bar Controller

---
+++ CinemaClub/MovieDetailViewController.swift
@@ -0,0 +1,11 @@
+//
+//  DetailViewController.swift
+//  CinemaClub
+//
+//  Created by Ugowe on 9/30/16.
+//  Copyright © 2016 Ugowe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
